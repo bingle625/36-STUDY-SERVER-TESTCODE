4a91dc9231efc020743779816b9056382667582f
bingle625 <fhqhxm8899@gmail.com>
2025-05-26 18:55:29 +0900

    test: 재고 로직 관련 서비스 단위 테스트 코드 작성

--- src/test/java/sopt/study/testcode/seongjae/api/service/order/OrderServiceTest.java
+++ src/test/java/sopt/study/testcode/seongjae/api/service/order/OrderServiceTest.java
@@ -3,6 +3,8 @@ package sopt.study.testcode.seongjae.api.service.order;
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static sopt.study.testcode.seongjae.domain.product.ProductSellingStatus.SELLING;
+import static sopt.study.testcode.seongjae.domain.product.ProductType.BAKERY;
+import static sopt.study.testcode.seongjae.domain.product.ProductType.BOTTLE;
 import static sopt.study.testcode.seongjae.domain.product.ProductType.HANDMADE;
 
 import java.time.LocalDateTime;
@@ -13,6 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 import sopt.study.testcode.seongjae.api.controller.order.request.OrderCreateRequest;
 import sopt.study.testcode.seongjae.api.service.order.response.OrderResponse;
 import sopt.study.testcode.seongjae.domain.order.OrderRepository;
@@ -20,9 +23,12 @@ import sopt.study.testcode.seongjae.domain.orderproduct.OrderProductRepository;
 import sopt.study.testcode.seongjae.domain.product.Product;
 import sopt.study.testcode.seongjae.domain.product.ProductRepository;
 import sopt.study.testcode.seongjae.domain.product.ProductType;
+import sopt.study.testcode.seongjae.domain.stock.Stock;
+import sopt.study.testcode.seongjae.domain.stock.StockRepository;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@Transactional
 class OrderServiceTest {
 
   @Autowired
@@ -37,12 +43,15 @@ class OrderServiceTest {
   @Autowired
   private OrderRepository orderRepository;
 
-  @AfterEach
-  void tearDown() {
-    orderProductRepository.deleteAllInBatch();
-    productRepository.deleteAllInBatch();
-    orderRepository.deleteAllInBatch();
-  }
+  @Autowired
+  private StockRepository stockRepository;
+
+//  @AfterEach
+//  void tearDown() {
+//    orderProductRepository.deleteAllInBatch();
+//    productRepository.deleteAllInBatch();
+//    orderRepository.deleteAllInBatch();
+//  }
 
   @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
   @Test
@@ -101,6 +110,52 @@ class OrderServiceTest {
         .containsExactlyInAnyOrder(
             tuple("001", 1000),
             tuple("001", 1000)
+        );
+  }
+
+  @DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트를 받아 주문을 생성한다.")
+  @Test
+  void createOrderWithStock() {
+    // given
+    final LocalDateTime registeredDateTime = LocalDateTime.now();
+
+    final Product product1 = createProduct(BOTTLE, "001", 1000);
+    final Product product2 = createProduct(BAKERY, "002", 3000);
+    final Product product3 = createProduct(HANDMADE, "003", 5000);
+    productRepository.saveAll(List.of(product1, product2, product3));
+
+    final OrderCreateRequest request = OrderCreateRequest.builder()
+        .productNumbers(List.of("001", "001", "002", "003"))
+        .build();
+
+    final Stock stock1 = Stock.create("001", 2);
+    final Stock stock2 = Stock.create("002", 2);
+    stockRepository.saveAll(List.of(stock1, stock2));
+
+    // when
+    OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+    // then
+    assertThat(orderResponse.getId()).isNotNull();
+    assertThat(orderResponse)
+        .extracting("registeredDateTime", "totalPrice")
+        .contains(registeredDateTime, 10000);
+
+    assertThat(orderResponse.getProducts()).hasSize(4)
+        .extracting("productNumber", "price")
+        .containsExactlyInAnyOrder(
+            tuple("001", 1000),
+            tuple("001", 1000),
+            tuple("002", 3000),
+            tuple("003", 5000)
+        );
+
+    final List<Stock> stocks = stockRepository.findAll();
+    assertThat(stocks).hasSize(2)
+        .extracting("productNumber", "quantity")
+        .containsExactlyInAnyOrder(
+            tuple("001", 0),
+            tuple("002", 1)
         );
   }
 
